54f26e72661eb080661e236eaeefaae050d6d126
Artur G Vieira <vieira.artur.g@gmail.com>
2017-12-11 04:38:19 +0000

    Feature menu layout rework complete

--- ui/src/toolbar/Toolbar.js
+++ ui/src/toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Toolbar.css';
+import Menu from './menu.svg';
 import Title from './components/title/Title.js';
 import Links from './components/links/Links.js';
 import Profile from './components/profile/Profile.js';
@@ -10,6 +11,9 @@ class Toolbar extends Component {
       <div className="Toolbar">
         <nav>
           <section className="wrapper">
+            <div className="menu">
+              <a href="javascript:void()"><img src={Menu} alt="Menu" /></a>
+            </div>
             <Title />
             <Links />
             <Profile />
