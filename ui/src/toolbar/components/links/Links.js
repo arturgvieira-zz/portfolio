dea8bc807dac03e1426928c207944aaee579b91d
Artur G Vieira <vieira.artur.g@gmail.com>
2017-12-11 05:04:26 +0000

    Feature links security update complete

--- ui/src/toolbar/components/links/Links.js
+++ ui/src/toolbar/components/links/Links.js
@@ -6,8 +6,8 @@ class Links extends Component {
     return (
       <div className="Links">
         <span className="item"><a href="https://portfolio.arturgvieira.com">Home</a></span>
-        <span className="item"><a href="https://arturgvieira.com" target="_blank">Website</a></span>
-        <span className="item"><a href="https://arturgvieira.quip.com" target="_blank">Hire</a></span>
+        <span className="item"><a href="https://arturgvieira.com" rel="noopener noreferrer" target="_blank">Website</a></span>
+        <span className="item"><a href="https://arturgvieira.quip.com" rel="noopener noreferrer" target="_blank">Hire</a></span>
       </div>
     );
   }
