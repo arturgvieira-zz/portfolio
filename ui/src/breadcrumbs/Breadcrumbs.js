5ddde388397ccb0b139fa4f5a481cdb5a1ef7e07
Artur G Vieira <vieira.artur.g@gmail.com>
2017-12-11 02:14:43 +0000

    Feature Anchor Tag Rework complete

--- ui/src/breadcrumbs/Breadcrumbs.js
+++ ui/src/breadcrumbs/Breadcrumbs.js
@@ -5,7 +5,7 @@ class Breadcrumbs extends Component {
   render() {
     return (
       <div className="Breadcrumbs">
-        <a className="part" href="portfolio.arturgvieira.com"><h4>Home</h4></a> 
+        <a className="part" href="https://portfolio.arturgvieira.com"><h4>Home</h4></a> 
       </div>
     );
   }
