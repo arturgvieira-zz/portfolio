128cd0d69ea432b1aaa8a84de3c8363004a68ec0
Artur G Vieira <vieira.artur.g@gmail.com>
2017-12-12 12:05:35 +0000

    Feature update endpoints complete

--- functions/index.js
+++ functions/index.js
@@ -19,36 +19,36 @@ app.use(cors());
 app.get('/', function(req, res){
     // Get a database reference
     var db = admin.database();
-    var ref = db.ref("welcome");
+    var ref = db.ref("portfolio/data");
     ref.once("value", function(data) {
-      res.json({ message: data});
+      res.json(data);
     });
 });
 
 app.get('/projects', function(req, res){
     // Get a database reference
     var db = admin.database();
-    var ref = db.ref("portfolio/projects");
+    var ref = db.ref("portfolio/data/projects");
     ref.once("value", function(data) {
-      res.json({ message: data});
+      res.json(data);
     });
 });
 
 app.get('/frameworks', function(req, res){
     // Get a database reference
     var db = admin.database();
-    var ref = db.ref("portfolio/frameworks");
+    var ref = db.ref("portfolio/data/frameworks");
     ref.once("value", function(data) {
-      res.json({ message: data});
+      res.json(data);
     });
 });
 
 app.get('/languages', function(req, res){
     // Get a database reference
     var db = admin.database();
-    var ref = db.ref("portfolio/languages");
+    var ref = db.ref("portfolio/data/languages");
     ref.once("value", function(data) {
-      res.json({ message: data});
+      res.json(data);
     });
 });
 
