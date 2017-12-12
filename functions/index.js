1818223564d9bf54451fe8f0667f5491ea37d76c
Artur G Vieira <vieira.artur.g@gmail.com>
2017-12-12 04:35:56 +0000

    Feature server endpoint complete

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const functions = require("firebase-functions");
 const express = require("express");
-
+const cors = require("cors");
 /* Express */
 const app = express();
+app.use(cors());
+
 app.get('/', function(req, res){
-    res.sendfile('index.html', { root: __dirname + "/" } );
+    res.json({ message: 'Hello, from NodeJS Express Server Firebase Function.'});
 });
 
 const api = functions.https.onRequest(app);
